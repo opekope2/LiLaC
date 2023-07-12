d30de08657a4d3da015da71f73a9460a292e4c5d
opekope2 <opekope2@gmail.com>
2023-07-12 16:03:16 +0200

    Add ResourceNotFoundException

---
+++ Api/src/main/java/opekope2/lilac/exception/ResourceNotFoundException.java
@@ -0,0 +1,32 @@
+package opekope2.lilac.exception;
+
+import net.minecraft.util.Identifier;
+import opekope2.lilac.api.resource.IResource;
+import opekope2.lilac.api.resource.IResourceReader;
+
+/**
+ * Thrown when trying to access a resource, which doesn't exist.
+ *
+ * @see IResource#getResourcePack()
+ * @see IResourceReader#getInputStream()
+ */
+public final class ResourceNotFoundException extends Exception {
+    private final Identifier resource;
+
+    /**
+     * Creates a new {@link ResourceNotFoundException}.
+     *
+     * @param resource The resource ID, which was not found
+     */
+    public ResourceNotFoundException(Identifier resource) {
+        super("Resource '%s' doesn't exist.".formatted(resource));
+        this.resource = resource;
+    }
+
+    /**
+     * Returns the missing resource ID passed in the constructor.
+     */
+    public Identifier getResourceId() {
+        return resource;
+    }
+}
