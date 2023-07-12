3e33f9145a5a06f1f34c699ea73b0e569279d638
opekope2 <opekope2@gmail.com>
2023-07-12 18:00:48 +0200

    Clarify IResourceReader docs

--- Api/src/main/java/opekope2/lilac/api/resource/IResourceReader.java
+++ Api/src/main/java/opekope2/lilac/api/resource/IResourceReader.java
@@ -11,10 +11,10 @@ import java.io.InputStream;
 public interface IResourceReader extends IResource {
     /**
      * Returns the input stream the resource can be read from.
-     * After using the resource, it should be closed with {@link InputStream#close()}.
+     * After reading from the input stream, it should be closed with {@link InputStream#close()}.
      *
      * @throws IOException               If Minecraft throws it
-     * @throws ResourceNotFoundException If the given resource doesn't exist. Check with {@link #exists()}
+     * @throws ResourceNotFoundException If the given resource doesn't exist. Check with {@link #exists()} beforehand
      */
     InputStream getInputStream() throws IOException, ResourceNotFoundException;
 }
