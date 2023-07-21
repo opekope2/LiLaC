36e643f6270c1d3c00d6a1683cc9e8da2ff90495
opekope2 <opekope2@gmail.com>
2023-07-21 20:53:28 +0200

    Remove unnecessary dependencies and prepare tests

--- Api/build.gradle.kts
+++ Api/build.gradle.kts
@@ -11,11 +11,33 @@ group = project.extra["maven_group"] as String
 
 repositories {}
 
+loom {
+    createRemapConfigurations(sourceSets["test"])
+}
+
 dependencies {
     minecraft("com.mojang", "minecraft", project.extra["minecraft_version"] as String)
     mappings("net.fabricmc", "yarn", project.extra["yarn_mappings"] as String, null, "v2")
-    modImplementation("net.fabricmc", "fabric-loader", project.extra["loader_version"] as String)
+    modApi("net.fabricmc", "fabric-loader", project.extra["loader_version"] as String)
     modImplementation(fabricApi.module("fabric-lifecycle-events-v1", project.extra["fabric_version"] as String))
+
+    testImplementation("org.junit.jupiter", "junit-jupiter", "5.9.3")
+    testRuntimeOnly("org.junit.platform", "junit-platform-launcher")
+
+    testImplementation("net.fabricmc", "fabric-loader-junit", project.extra["loader_version"] as String)
+    testRuntimeOnly(project(":1.19.3", configuration = "namedElements"))
+
+    "modTestRuntimeOnly"(fabricApi.module("fabric-resource-loader-v0", project.extra["fabric_version"] as String))
+    "modTestRuntimeOnly"(
+        "net.fabricmc", "fabric-language-kotlin", project.extra["fabric_language_kotlin_version"] as String
+    )
+}
+
+val packageTestMod by tasks.creating(Jar::class) {
+    from(sourceSets["test"].resources)
+    include("fabric.mod.json")
+    archiveClassifier = "testmod"
+    destinationDirectory = File(project.buildDir, "testlibs")
 }
 
 tasks {
@@ -29,7 +51,7 @@ tasks {
     }
 
     jar {
-        from("LICENSE") {
+        from(File(rootDir, "LICENSE")) {
             rename { "${it}_${base.archivesName.get()}" }
         }
     }
@@ -40,7 +62,10 @@ tasks {
                 mutableMapOf(
                     "version" to project.extra["mod_version"] as String,
                     "fabricloader" to project.extra["loader_version"] as String,
-                    "fabric_api" to project.extra["fabric_version"] as String,
+                    "fabric_lifecycle_events_v1" to fabricApi.moduleVersion(
+                        "fabric-lifecycle-events-v1",
+                        project.extra["fabric_version"] as String
+                    ),
                     "minecraft" to project.extra["minecraft_version"] as String,
                     "java" to project.extra["java_version"] as String
                 )
@@ -55,5 +80,21 @@ tasks {
         sourceCompatibility = javaVersion
         targetCompatibility = javaVersion
         withSourcesJar()
+    }
+
+    test {
+        dependsOn(packageTestMod)
+
+        useJUnitPlatform()
+        workingDir(project.file("run"))
+        classpath += packageTestMod.outputs.files
+
+        doFirst {
+            mkdir(workingDir)
+        }
+
+        testLogging {
+            events("PASSED", "SKIPPED", "FAILED")
+        }
     }
 }
