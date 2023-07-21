5a9b314136ae9306eb6569378ab2dcfe5b31c09f
opekope2 <opekope2@gmail.com>
2023-07-21 20:55:59 +0200

    Create common code project for implementations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
     kotlin("jvm") apply false
 }
+
+subprojects {
+    configurations.all {
+        resolutionStrategy {
+            val loader_version: String by project.extra
+            force("net.fabricmc:fabric-loader:$loader_version")
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,4 +12,5 @@ pluginManagement {
 
 include(
     "Api",
+    "Common",
 )
